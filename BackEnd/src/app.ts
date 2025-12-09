4d54e37898e2b0cd770efefa126999823a783614
Erik Borges da Silva <erik.silvadesenvolvedor@gmail.com>
2025-12-09 11:47:29 -0300

    Cors habilitado

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,18 +1,18 @@
-import fastify, { type FastifyInstance } from "fastify";
+import cors from '@fastify/cors';
+import fastify, { type FastifyInstance } from 'fastify';
 import { env } from 'process';
 import routes from './routes';
 import transactionsRoutes from './routes/transactions.routes';
 
-const app: FastifyInstance = fastify(
-  {
-    logger: {
-      level: env.NODE_ENV === "development" ? 'info' : "error",
-    },
-  }
-);
+const app: FastifyInstance = fastify({
+  logger: {
+    level: env.NODE_ENV === 'development' ? 'info' : 'error',
+  },
+});
 
-app.register(routes, {prefix: "/api"});
-app.register(transactionsRoutes, {prefix: "/transactions"});
+app.register(cors);
 
+app.register(routes, { prefix: '/api' });
+app.register(transactionsRoutes, { prefix: '/transactions' });
 
 export default app;
