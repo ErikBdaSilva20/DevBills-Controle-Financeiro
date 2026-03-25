825fcd9987908c8ed4540489793b152508a6123c
Erik Borges da Silva <erik.silvadesenvolvedor@gmail.com>
2026-03-25 17:34:05 -0300

    added configuration to secret file in render

--- BackEnd/src/config/firebase.ts
+++ BackEnd/src/config/firebase.ts
@@ -1,22 +1,33 @@
 import admin from 'firebase-admin';
-import { env } from './env';
+import { env } from './env.js';
+import fs from 'node:fs';
+import path from 'node:path';
 
 const initializeFirebaseAdmin = (): void => {
   if (admin.apps.length > 0) return;
 
-  const { FIREBASE_CLIENT_EMAIL, FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY } = env;
+  const serviceAccountPath = path.resolve(process.cwd(), 'fireBaseServiceAccount.json');
 
-  if (!FIREBASE_PROJECT_ID || !FIREBASE_PRIVATE_KEY || !FIREBASE_CLIENT_EMAIL) {
-    throw new Error('FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL is required');
-  }
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: FIREBASE_PROJECT_ID,
-        clientEmail: FIREBASE_CLIENT_EMAIL,
-        privateKey: FIREBASE_PRIVATE_KEY,
-      }),
-    });
+    if (fs.existsSync(serviceAccountPath)) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccountPath),
+      });
+    } else {
+      const { FIREBASE_CLIENT_EMAIL, FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY } = env;
+
+      if (!FIREBASE_PROJECT_ID || !FIREBASE_PRIVATE_KEY || !FIREBASE_CLIENT_EMAIL) {
+        throw new Error('Firebase Service Account file or environment variables (FIREBASE_PROJECT_ID, etc.) are required');
+      }
+
+      admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: FIREBASE_PROJECT_ID,
+          clientEmail: FIREBASE_CLIENT_EMAIL,
+          privateKey: FIREBASE_PRIVATE_KEY,
+        }),
+      });
+    }
   } catch (error) {
     process.exit(1);
   }
